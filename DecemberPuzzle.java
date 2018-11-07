62a1eafeb336d797c771b31322901a0f6a25ca6a
TheRealMerlin <iankuffel@gmail.com>
2018-11-06 23:40:58 -0600

    encrypt finished

--- DecemberPuzzle.java
+++ DecemberPuzzle.java
@@ -6,8 +6,8 @@ public class DecemberPuzzle {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new File("decemberEND.dat"));
 		String plain = in.nextLine();
-		int[] vkey = makeVKey("stupendeous");
-		String vEnc = "stupendeous - ";
+		int[] vkey = makeVKey("celebration");
+		String vEnc = "celebration - ";
 		for(int i = 0; i < plain.length(); i++) {
 			char c = plain.charAt(i);
 			char newC = (char)(c + vkey[i%vkey.length]);
@@ -26,13 +26,45 @@ public class DecemberPuzzle {
 			}
 		}
 		out.println(vEnc);
-		String hexEnc = "";
+		ArrayList<Integer> vEncNum = new ArrayList<Integer>();
 		for(int i = 0; i < vEnc.length(); i++) {
-			vEnc += Integer.toString(Integer.parseInt(vEnc.substring(i, i+1), 16));
+			vEncNum.add((int)vEnc.charAt(i));
 		}
-		out.println(hexEnc);
-		int col = 13;
-		int row = 9;
+		int[][] mKey = {
+				{'h', 'a', 'p', 'p', 'y'}, 
+				{'h', 'o', 'l', 'l', 'y'},
+				{'h', 'o', 'l', 'i', 'd'},
+				{'a', 'y', 'c', 'a', 'r'},
+				{'o', 'l', 'e', 'r', 's'}};
+		int[][] mUnc = new int[7][5];
+		int[][] mEnc = new int[7][5];
+		int vEncNumPos = 0;
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 5; j++) {
+				mUnc[i][j] = vEncNum.get(vEncNumPos);
+				vEncNumPos++;
+			}
+		}
+		for(int x = 0; x < 7; x++) {
+			for(int i = 0; i < 5; i++) {
+				int sum = 0;
+				for(int j = 0; j < 5; j++) {
+					sum += mUnc[x][j] * mKey[j][i];
+				}
+				mEnc[x][i] = sum;
+			}
+		}
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 5; j++) {
+				mEnc[i][j] = mEnc[i][j] % 256;
+			}
+		}
+		for(int[] a : mEnc) {
+			for(int b : a) {
+				out.print(Integer.toString(b, 16) + " ");
+			}
+			out.println();
+		}
 	}
 	
 	public static int[] makeVKey(String str) {
